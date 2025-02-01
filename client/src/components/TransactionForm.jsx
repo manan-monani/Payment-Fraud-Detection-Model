47ae02043cff5098da0bdfbd065ab52a4945a827
Manan <mmmonani747@gmail.com>
2025-02-01 14:14:42 +0530

    some changes into transaction form.

--- client/src/components/TransactionForm.jsx
+++ client/src/components/TransactionForm.jsx
@@ -57,7 +57,7 @@ const TransactionForm = ({ onTransactionSuccess, token }) => {
                deviceID: deviceId,
                time: Math.floor(Date.now() / 1000)
            };
-
+            // this is first commite to transaction form.
            try {
                // ML model API
                const mlResponse = await fetch('http://127.0.0.1:5000/predict', {
